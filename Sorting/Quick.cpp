10a46187dc1f24d7d743475fc76d17a00a350e7f
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2025-03-03 22:37:19 +0600

    Quick.cpp

--- Sorting/Quick.cpp
+++ Sorting/Quick.cpp
@@ -1,14 +1,10 @@
-
-
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define str string
-ll Part(vector<ll> &arr, ll low, ll high)
+int Partition(vector<int> &arr, int low, int high)
 {
-    ll pivot = arr[low];
-    ll i = low;
-    ll j = high;
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
     while (i < j)
     {
         while (arr[i] <= pivot and i <= high - 1)
@@ -27,41 +23,30 @@ ll Part(vector<ll> &arr, ll low, ll high)
     swap(arr[low], arr[j]);
     return j;
 }
-vector<ll> qsort(vector<ll> &arr, ll low, ll high)
+vector<int> qsort(vector<int> &arr, int low, int high)
 {
     if (low < high)
     {
-        ll pi = Part(arr, low, high);
+        int pi = Partition(arr, low, high);
         qsort(arr, low, pi - 1);
         qsort(arr, pi + 1, high);
     }
     return arr;
 }
-void solve()
+int main()
 {
-    // By Farhadul Islam
-    // Only Brute Force Bhai
 
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> a(n);
-    for (ll i = 0; i < n; i += 1)
+    vector<int> a(n);
+    for (int i = 0; i < n; i += 1)
     {
         cin >> a[i];
     }
-    vector<ll> ans;
+
     qsort(a, 0, n - 1);
     for (auto x : a)
     {
         cout << x << " ";
-    }
-}
-int32_t main()
-{
-    int t = 1;
-    // cin >> t;
-    while (t--)
-    {
-        solve();
     }
 }
