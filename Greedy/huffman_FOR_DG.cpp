d57fb7bf95f458647c423b4a558f262e43d5edc7
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2025-06-30 22:18:06 +0600

    huffman_FOR_DG.cpp

--- Greedy/huffman_FOR_DG.cpp
+++ Greedy/huffman_FOR_DG.cpp
@@ -25,13 +25,13 @@ struct Sort
     }
 };
 
-void f(F *matha, string cd, unordered_map<char, string> &code)
+void f(F *matha, string cd, map<char, string> &code)
 {
     if (matha == nullptr)
         return;
     if (matha->l == nullptr && matha->r == nullptr)
     {
-        code[matha->c] = cd;
+        code[matha->c] = cd.empty() ? "0" : cd;
     }
     f(matha->l, cd + '0', code);
     f(matha->r, cd + '1', code);
@@ -39,46 +39,65 @@ void f(F *matha, string cd, unordered_map<char, string> &code)
 
 void hfman(string &s)
 {
-    unordered_map<char, long long> hs;
+    map<char, long long> hs;
     for (auto cc : s)
-    {
         hs[cc]++;
-    }
+
     priority_queue<F *, vector<F *>, Sort> pq;
-    for (auto cc : hs)
-    {
+    for (auto &cc : hs)
         pq.push(new F(cc.first, cc.second));
-    }
+
     while (pq.size() != 1)
     {
         F *l = pq.top();
         pq.pop();
         F *r = pq.top();
         pq.pop();
-        long long sum = l->cnt + r->cnt;
-        F *ss = new F('*', sum);
+        F *ss = new F('*', l->cnt + r->cnt);
         ss->l = l;
         ss->r = r;
         pq.push(ss);
     }
 
     F *matha = pq.top();
-    unordered_map<char, string> code;
+    map<char, string> code;
     f(matha, "", code);
 
+    cout << "Huffman Codes:\n";
     for (auto cc : code)
     {
-        cout << cc.first << " " << cc.second << "\n";
+        cout << cc.first << ": " << cc.second << "\n";
     }
 
     string ans = "";
-    for (auto cc : s)
+    for (auto ch : s)
     {
-        cout << code[cc] << " ";
-        ans += code[cc];
+        ans += code[ch];
     }
-    cout << "\n";
-    cout << ans << "\n";
+
+    long long mb = 0;
+    vector<string> V;
+    int x = 0;
+    cout << "\nMessage bits = ";
+    for (auto ch : hs)
+    {
+
+        if (x > 0)
+            cout << " + ";
+        long long cc = ch.second * code[ch.first].length();
+        cout << to_string(ch.second) + "*" + to_string(code[ch.first].length());
+        mb += cc;
+        x++;
+    }
+
+    cout << " = " << mb << " bits\n";
+
+    long long tb = 0;
+    for (auto ch : code)
+        tb += (8 + ch.second.length());
+
+    cout << "Table bits   = " << tb << " bits\n";
+    cout << "Total bits   = " << (mb + tb) << " bits\n";
 }
 
 int main()
